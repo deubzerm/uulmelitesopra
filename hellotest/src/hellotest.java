e5d81d9bf895cd8b6a7121a3532def659a411129
unknown <Onze@.(none)>
2012-10-24 19:59:31 +0200

    2. test, wegen der sicherheit

--- hellotest/src/hellotest.java
+++ hellotest/src/hellotest.java
@@ -7,6 +7,8 @@ public class hellotest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		System.out.println("Felix ist doof");
 
 	}
 
